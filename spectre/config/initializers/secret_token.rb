669cb128d7a2cff4f6f7f81ba046b7ab6e214247
Brian <kikbal@decoytech.com>
2013-01-14 07:58:39 -0600

    initial commit

---
+++ spectre/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spectre::Application.config.secret_token = '91a4a834899aad286a671bb9537ea69853fb078c3c8a248ce0113e00219be1cde298fe732bf8927d13cf3d00aae533eb1b85beb6b9d3e963a7d2b79bb3f05e0d'
